5a3cfeffc9edd167cf88f4bfa582147a983a4772
William Goeller <william@williamgoeller.com>
2019-10-24 01:16:16 -0400

    Cleaned out comments

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM debian:buster
 RUN (apt-get update && apt-get install -y git live-build curl coreutils vim && \
   mkdir /opt/live)
-#  mkdir /opt/live #&& mkdir /root/tmp)
-#ADD . /root/tmp
 ADD . /opt/live
 CMD /bin/bash /opt/live/build.sh
-#CMD /bin/bash /root/tmp/build.sh
--- build.sh
+++ build.sh
@@ -2,11 +2,7 @@
 set -e
 echo "Starting Ensign Live Build"
 cd /opt/live/
-#echo "$(ls .)"
 lb config
-#echo "$(ls .)"
-#cp /root/tmp/auto/config /opt/live/auto
-#lb config
 echo "Patching debootstrap"
 patch /usr/share/debootstrap/scripts/debian-common </opt/live/debootstrap.diff
 lb build
